df2bcac1ae9806f5215373d7e5acc7268465e26c
Yash Bedekar <128920318+yashbedekar07@users.noreply.github.com>
2024-09-24 12:32:49 +0530

    Add files via upload

---
+++ Exception-Handling/Program.cs
@@ -0,0 +1,39 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("This an Exception Handling Example...");
+
+        CheckAge(12);
+
+        try
+        {
+            int a = 7;
+            int b = 34;
+            int x = a + b;
+            Console.WriteLine(x);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+        }
+        finally
+        {
+            Console.WriteLine("The Code is Finish..");
+        }
+        Console.WriteLine("Something Went Wrong....");
+    }
+
+    public static void CheckAge(int age)
+    {
+        if (age < 18)
+        {
+            throw new ArithmeticException("Access not granted because Your not old enough");
+        }
+        else
+        {
+            Console.WriteLine("Acess granted..");
+        }
+        Console.ReadKey();
+    }
+}
---
+++ Inheritance/Program.cs
@@ -0,0 +1,44 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("This is an Example of Inheritance..");
+
+        // Creating an instance of the base class
+        Animal myAnimal = new Animal();
+        myAnimal.Eat();   // Output: Animal is eating
+        myAnimal.Sleep(); // Output: Animal is sleeping
+
+        Console.WriteLine();
+
+        // Creating an instance of the derived class
+        Dog myDog = new Dog();
+        myDog.Eat();      // Output: Animal is eating (inherited from base class)
+        myDog.Sleep();    // Output: Animal is sleeping (inherited from base class)
+        myDog.Bark();     // Output: Dog is barking (specific to Dog class)
+
+        Console.ReadLine();
+    }
+}
+
+class Animal
+{
+    public void Eat()
+    {
+        Console.WriteLine("Animal is eating");
+    }
+
+    public void Sleep()
+    {
+        Console.WriteLine("Animal is sleeping");
+    }
+}
+
+// Derived class (inherits from Animal)
+class Dog : Animal
+{
+    public void Bark()
+    {
+        Console.WriteLine("Dog is barking");
+    }
+}
