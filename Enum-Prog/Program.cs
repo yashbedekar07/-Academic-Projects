f1452d6b42490fdb2f5cfa07508a9a35127905ee
Yash Bedekar <128920318+yashbedekar07@users.noreply.github.com>
2024-09-24 12:34:35 +0530

    Add files via upload

---
+++ Enum-Prog/Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualBasic;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello Yash!!");
+        Console.WriteLine(WeekDays.Monday); // Monday
+        Console.WriteLine(WeekDays.Tuesday); // Tuesday
+        Console.WriteLine(WeekDays.Wednesday); // Wednesday
+        Console.WriteLine(WeekDays.Thursday); // Thursday
+        Console.WriteLine(WeekDays.Friday); // Friday
+        Console.WriteLine(WeekDays.Saturday); // Saturday
+        Console.WriteLine(WeekDays.Sunday); // Sunday
+    }
+    enum WeekDays
+    {
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday
+    }
+}
